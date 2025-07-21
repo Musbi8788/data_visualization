ee7f126a1fee6c80c6f4120f0820e3159e117df9
Musbi <musbi700@gmail.com>
2025-07-21 23:18:13 +0100

    plot chart shown

---
+++ mpl_squares.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25]
+
+fig, ax = plt.subplots() # fig represent figure, ax represent single plot in the figure
+ax.plot(squares, linewidth=3) # plot is used for plotting the data in a meaningful way. linewidth represent the plot line width
+
+# Set chart title and label, axes
+ax.set_title("Square Numbers", fontsize=14) #  the chart title
+ax.set_xlabel("Value", fontsize=14) # the x label 
+ax.set_ylabel("Square of Value", fontsize=14) # the y label
+
+# Set size of tick labels
+ax.tick_params(axis='both', labelsize=14) # axis both apply the effect on both x and y ``
+
+
+plt.show() # display the matplotlib viewer
--- note.md
+++ note.md
@@ -1,1 +1,3 @@
 # Generating date
+ * The pyplot module contains a number of functions that generate charts and plots
+* subplot can generate one or more plot in the same figure.
