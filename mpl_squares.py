6c332304de39bdafbdefb75873a12819a0fe96ae
Musbi <musbi700@gmail.com>
2025-07-22 22:42:03 +0100

    scatter method add

--- mpl_squares.py
+++ mpl_squares.py
@@ -3,16 +3,21 @@ import matplotlib.pyplot as plt
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
+# Using a built in styles
+plt.style.use('bmh')
+
 fig, ax = plt.subplots() # fig represent figure, ax represent single plot in the figure
-ax.plot(input_values, squares, linewidth=3) # plot is used for plotting the data in a meaningful way. linewidth represent the plot line width
+
+ax.scatter(2, 4, s=200) # s adjust the dot size
+# ax.plot(input_values, squares, linewidth=3) # plot is used for plotting the data in a meaningful way. linewidth represent the plot line width
 
 # Set chart title and label, axes
-ax.set_title("Square Numbers", fontsize=14) #  the chart title
+ax.set_title("Square Numbers", fontsize=24) #  the chart title
 ax.set_xlabel("Value", fontsize=14) # the x label 
 ax.set_ylabel("Square of Value", fontsize=14) # the y label
 
 # Set size of tick labels
-ax.tick_params(axis='both', labelsize=14) # axis both apply the effect on both x and y ``
+ax.tick_params(axis='both', which='major' ,labelsize=14) # axis both apply the effect on both x and y ``
 
 
 plt.show() # display the matplotlib viewer
--- note.md
+++ note.md
@@ -2,6 +2,15 @@
  * The pyplot module contains a number of functions that generate charts and plots
 * subplot can generate one or more plot in the same figure.
   
-# Correnting the plot x coodinate
+### Correnting the plot x coodinate
 you can change the x conodinat by generating your own like this 
 ```.plot(input_values, squares, linewidth=3)```
+
+### built in stles in plot
+to check the avaible built in styles run 
+```>>> import matplotlib.pyplot as plt```
+```>>> plt.style.available```
+
+### Plotting and Styling Individual Points with scatter()
+this method show a dot on a specific in the chart and to make it apply that the only thing you do is to pass x-y values
+```ax.scatter(2, 4)```
