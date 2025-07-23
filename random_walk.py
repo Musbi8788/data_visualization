fff5f0defb5c419128e1a65d93310363da11b445
Musbi <musbi700@gmail.com>
2025-07-24 00:14:53 +0100

    refac

--- random_walk.py
+++ random_walk.py
@@ -1,6 +1,6 @@
 from random import choice
 
-class RamdonWalk():
+class RandomWalk():
     """A class to generate random walks."""
 
     def __init__(self, num_points=5000):
@@ -14,18 +14,27 @@ class RamdonWalk():
     def fill_walk(self,):
         """Calculate all the points in the walk."""
 
+        
+        self.x = self.x_values.get_step()
+        self.y = self.y_values.get_step()
+
+    def get_step(self):
+        """Determine the direction and distance of each step.
+        """
         # Keep taking steps until the walk reaches the desired length
         while len(self.x_values) < self.num_points:
 
             # Decide which direction to go and how far to go in that direction
 
-            x_direction = choice([1, -1]) # move right when positive , move left when negative, and 0 move vertical
-            x_distance = choice([0, 1, 2, 3, 4])
+            # move right when positive , move left when negative, and 0 move vertical
+            x_direction = choice([1])
+            x_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
             x_step = x_direction * x_distance
 
             # move right when positive , move left when negative, 0 move horizontal
             y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4]) # choose random distance from 0 4
+            # choose random distance from 0 4
+            y_distance = choice([0, 1, 2, 3, 4])
             y_step = y_direction * y_distance
 
             # Reject moves that go nowhere
@@ -39,4 +48,3 @@ class RamdonWalk():
             # Store the new positions
             self.x_values.append(x)
             self.y_values.append(y)
-
