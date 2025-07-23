beefe2eea3fe8d1df1c1b2c4140d471ab743cee8
Musbi <musbi700@gmail.com>
2025-07-24 00:45:51 +0100

    finished exercise

--- random_walk.py
+++ random_walk.py
@@ -13,29 +13,11 @@ class RandomWalk():
 
     def fill_walk(self,):
         """Calculate all the points in the walk."""
-
-        
-        self.x = self.x_values.get_step()
-        self.y = self.y_values.get_step()
-
-    def get_step(self):
-        """Determine the direction and distance of each step.
-        """
         # Keep taking steps until the walk reaches the desired length
         while len(self.x_values) < self.num_points:
-
-            # Decide which direction to go and how far to go in that direction
-
-            # move right when positive , move left when negative, and 0 move vertical
-            x_direction = choice([1])
-            x_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-            x_step = x_direction * x_distance
-
-            # move right when positive , move left when negative, 0 move horizontal
-            y_direction = choice([1, -1])
-            # choose random distance from 0 4
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
+        
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             # Reject moves that go nowhere
             if x_step == 0 and y_step == 0:
@@ -48,3 +30,18 @@ class RandomWalk():
             # Store the new positions
             self.x_values.append(x)
             self.y_values.append(y)
+
+
+    def get_step(self):
+        """Determine the direction and distance of each step.
+        """
+        # Decide which direction to go and how far to go in that direction
+
+        # move right when positive , move left when negative, and 0 move vertical
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+        return direction * distance
+
+        
+
+
