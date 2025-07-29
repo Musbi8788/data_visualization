4060fd88e5fc0b8eb9edf3af0cd75de66863372b
Musbi <musbi700@gmail.com>
2025-07-29 22:27:46 +0100

    sikta death_valley high updated

--- exercises/sikta__death_valley_highs.py
+++ exercises/sikta__death_valley_highs.py
@@ -10,12 +10,12 @@ with open(file_name) as f:
     header_row = next(reader) # get first line for the file
     
     # Get dates high temperatures from this sitka.
-    dates, sitka_highs = [], []
+    sk_dates, sitka_highs = [], []
     # Loop through the entire file and get the max_tem values
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         sk_high = int(row[5]) # Get the MaxTem results 
-        dates.append(current_date)
+        sk_dates.append(current_date)
         sitka_highs.append(sk_high)
     
 
@@ -28,7 +28,7 @@ with open(filename) as f:
     #     print(index, column_reader)
 
     # Get dates, and high and low tempratures for this file.
-    dates, death_valley_highs, = [], []
+    dv_dates, death_valley_highs, = [], []
     for row in reader:
         current_date = datetime.strptime(row[2], "%Y-%m-%d")
         try:
@@ -37,7 +37,7 @@ with open(filename) as f:
         except ValueError:
             print(f"Missing date for {current_date}")
         else:
-            dates.append(current_date)
+            dv_dates.append(current_date)
             death_valley_highs.append(dv_high)
             
 
@@ -45,16 +45,18 @@ with open(filename) as f:
     # Plot the sitka and death valley high temperature.
     plt.style.use('seaborn-v0_8')
     fig, ax = plt.subplots()
-    ax.plot(dates, sitka_highs, c='red', alpha=0.5) # display the dates and highs
-    ax.plot(dates, death_valley_highs, c='blue', alpha=0.5)
+    ax.plot(sk_dates, sitka_highs, c='red', alpha=0.5, label='Sitka') # display the dates and highs
+    ax.plot(dv_dates, death_valley_highs, c='blue', alpha=0.5, label='Death Valley')
+    ax.legend()
+    ax.set_ylim(20, 130)
 
     # Format Plot
     plt.title("Daily Sitka and Death Valley high temperatures - 2018", fontsize=20)
     plt.xlabel('', fontsize=16)
-    fig.autofmt_xdate() # draw the date dagonally
+    fig.autofmt_xdate() # draw the date diagonally
     plt.ylabel("Temperature (F)", fontsize=16)
     plt.tick_params(axis='both', which='major', labelsize=16)
-    plt.fill_between(dates, sitka_highs, death_valley_highs, facecolor='blue', alpha=0.1) # Add a shading between sitka and death vally highs fill.
-    plt.savefig('sikta_death_valley_high_tem.png')
+    plt.fill_between(sk_dates, sitka_highs, death_valley_highs, facecolor='blue', alpha=0.1) # Shade the area between Sitka and Death Valley highs fill.
+    plt.savefig('sitka_death_valley_high_temps.png')
     plt.show()
 
