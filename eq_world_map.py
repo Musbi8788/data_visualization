031c820813cad4da78b280c7f9f11bb061d59820
Musbi <musbi700@gmail.com>
2025-07-31 21:20:05 +0100

    shows colorscale available

--- eq_world_map.py
+++ eq_world_map.py
@@ -1,10 +1,10 @@
 import json
 
-from plotly.graph_objs import Scattergeo, Layout
+from plotly.graph_objs import Layout
 from plotly import offline
 
 # Explore the structure of the data
-filename = 'data/eq_1_day_m1.json'
+filename = 'data/eq_data_30_days_m1.json'
 with open(filename, encoding='utf-8') as f:
     all_eq_data = json.load(f)  # store the entire json data
 
@@ -26,9 +26,16 @@ data = [
         'lon': lons,
         'lat': lats,
         'marker': {
-            'size': [5*mag for mag in mags],
+            'size': [3*mag for mag in mags],
+            'color': mags, # Detemind colour used
+            'colorscale': 'Rainbow',  # metermind which color to display
+            'reversescale': True, # determind how the color will be reversed
+            'colorbar': {
+                'title': 'Magnitude' # Display title on the sidebar color
+                        },
             },
-    }
+        
+    },
 ]
 my_layout = Layout(title='Global Earthquakes') # set the map title
 
